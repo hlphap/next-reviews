b7d8ba8ae8e01b26165ed88faa0c2a32353d43e6
Phap Huynh <phaphl@luminpdf.com>
2023-08-12 15:18:02 +0700

    ✨ Feat: navigation menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface IProps {
   children: React.ReactNode
 }
@@ -6,7 +8,21 @@ export default function Root({ children }: IProps) {
   return (
     <html lang="en">
       <body>
-      <header style={{border: "solid blue 1px"}}>[Header]</header>
+      <header style={{border: "solid blue 1px"}}>
+         <nav>
+          <ul>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/reviews/hollow-knight">Hollow knight</Link>
+            </li>
+            <li>
+              <Link href="/reviews/stardew-valley">Stardew valley</Link>
+            </li>
+          </ul>
+         </nav>
+      </header>
       {children}
       <footer style={{border: "solid green 1px"}}>[Footer]</footer>
       </body>
