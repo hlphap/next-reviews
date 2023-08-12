a8acf9b993193b18d982299586a269abfdbbabc0
Phap Huynh <phaphl@luminpdf.com>
2023-08-12 15:15:02 +0700

    ✨ Feat: client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+/**
+ * You're importing a component that needs useEffect. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
+  Learn more: https://nextjs.org/docs/getting-started/react-essentials
+ */
+'use client'
+import { useEffect } from "react"
+
 export default function Homepage() {
   console.log("Homepage rendering")
+
+  useEffect(() => {
+    console.log("hehe")
+  }, [])
   return (
     <>
       <h1>This is homepage</h1>
